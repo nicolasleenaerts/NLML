a2550e2f2102b55cfd38da2a696d8f249446b62c
Nicolas Leenaerts <58860006+mikojeske@users.noreply.github.com>
2023-04-18 21:19:53 +0200

    Update elastic_net_wrapper.R

--- elastic_net_wrapper.R
+++ elastic_net_wrapper.R
@@ -1,6 +1,6 @@
 elastic_net_wrapper <- function(data, outcome=NULL, predictors_con=NULL,predictors_cat=NULL, split=80, outer_cv=NULL, stratified=T,scaling=T,
-                                repeated_cv=1,ensr_cv=10,ensr_alphas=seq(0, 1, length = 10),ensr_lambdas=100,seed=404,shuffle=T,
-                                stop_train=NULL,stop_test=NULL){
+                                         repeated_cv=1,ensr_cv=10,ensr_alphas=seq(0, 1, length = 10),ensr_lambdas=100,seed=404,shuffle=T,
+                                         stop_train=NULL,stop_test=NULL,family='binary',pred_min=NULL,pred_max=NULL){
   # required packages
   require(ensr)
   require(glmnet)
@@ -28,18 +28,32 @@ elastic_net_wrapper <- function(data, outcome=NULL, predictors_con=NULL,predicto
   if (is.null(outer_cv)==T){
     # performing stratified split
     if(stratified==T){
-      # get indices
-      set.seed(seed)
-      my_train_ind_no_y =  sample(which(y==0), size = split/100*length(which(y==0)))
-      set.seed(seed)
-      my_train_ind_y =  sample(which(y==1), size = split/100*length(which(y==1)))
-      # split data
-      y_train = y[c(my_train_ind_no_y,my_train_ind_y),]
-      y_test =y[-c(my_train_ind_no_y,my_train_ind_y),]
-      x_train = x[c(my_train_ind_no_y,my_train_ind_y),]
-      x_test = x[-c(my_train_ind_no_y,my_train_ind_y),]
-      # add to analysis list
-      analysis_list[[1]] = list(y_train,y_test,x_train,x_test)
+      if (family=='binary'){
+        # get indices
+        set.seed(seed)
+        my_train_ind_no_y =  sample(which(y==0), size = split/100*length(which(y==0)))
+        set.seed(seed)
+        my_train_ind_y =  sample(which(y==1), size = split/100*length(which(y==1)))
+        # split data
+        y_train = y[c(my_train_ind_no_y,my_train_ind_y),]
+        y_test =y[-c(my_train_ind_no_y,my_train_ind_y),]
+        x_train = x[c(my_train_ind_no_y,my_train_ind_y),]
+        x_test = x[-c(my_train_ind_no_y,my_train_ind_y),]
+        # add to analysis list
+        analysis_list[[1]] = list(y_train,y_test,x_train,x_test)
+      }
+      else if (family=='continuous'){
+        # get indices
+        set.seed(seed)
+        my_train_ind =  createDataPartition(as.matrix(y), p = split/100, list = T,groups=min(3,nrow(y)))
+        # split data
+        y_train = y[c(unlist(my_train_ind)),]
+        y_test =y[-c(unlist(my_train_ind)),]
+        x_train = x[c(unlist(my_train_ind)),]
+        x_test = x[-c(unlist(my_train_ind)),]
+        # add to analysis list
+        analysis_list[[1]] = list(y_train,y_test,x_train,x_test)
+      }
     }
     # performing non-stratified split
     else if(stratified==F){
@@ -82,9 +96,16 @@ elastic_net_wrapper <- function(data, outcome=NULL, predictors_con=NULL,predicto
   }
   
   # creating the results dataframe
-  results_df = data.frame(matrix(ncol = (11+length(predictors))))
-  colnames(results_df) = c('fold','nrow_train','nrow_test','ny_train','ny_test','AUC','sensitivity','specificity',
-                           'accuracy','PPV','NPV',predictors)
+  if (family==('binary')){
+    results_df = data.frame(matrix(ncol = (11+length(predictors))))
+    colnames(results_df) = c('fold','nrow_train','nrow_test','ny_train','ny_test','AUC','sensitivity','specificity',
+                             'accuracy','PPV','NPV',predictors)
+  }
+  if (family==('continuous')){
+    results_df = data.frame(matrix(ncol = (10+length(predictors))))
+    colnames(results_df) = c('fold','nrow_train','nrow_test','mean_y_train','mean_y_test','R2','R2_adjusted','RMSE','MSE',
+                             'MAE',predictors)
+  }
   
   # Create progress bar
   print('Training and evaluating the models')
@@ -136,7 +157,7 @@ elastic_net_wrapper <- function(data, outcome=NULL, predictors_con=NULL,predicto
     # finding best lambda and alpha
     
     # creating a variable for storing the crossvalidation results for the alphas and the lambdas
-    MSEs <- NULL
+    MSEs = NULL
     
     # store variables for  ensr
     x_train_entry <<- x_train_entry
@@ -145,46 +166,49 @@ elastic_net_wrapper <- function(data, outcome=NULL, predictors_con=NULL,predicto
     ensr_cv <<- ensr_cv
     ensr_alphas <<- ensr_alphas
     
+    # get ensr family
+    ensr_family <<- ifelse(family=='binary','binomial','gaussian')
+    
     for (repeated_cv_number in 1:repeated_cv){
       
       # setting the seed
       set.seed(repeated_cv_number)
       # selecting the best alpha and lambda for this seed
-      ensr_obj <- ensr(y =data.matrix(y_train_entry), x = x_train_entry,nlambda=ensr_lambdas,nfolds = ensr_cv,
-                       alphas = ensr_alphas,family='binomial',standardize = F)
-      ensr_obj_summary <- summary(object = ensr_obj)
+      ensr_obj = ensr(y =data.matrix(y_train_entry), x = x_train_entry,nlambda=ensr_lambdas,nfolds = ensr_cv,
+                      alphas = ensr_alphas,family=ensr_family,standardize = F)
+      ensr_obj_summary = summary(object = ensr_obj)
       
       # storing the results
-      MSEs <- cbind(MSEs,ensr_obj_summary$cvm)
+      MSEs = cbind(MSEs,ensr_obj_summary$cvm)
     }
     
     # converting the cross validation results to a dataframe
-    MSEs <- as.data.frame(MSEs)
-    MSEs$rowMeans <- rowMeans(MSEs)
+    MSEs = as.data.frame(MSEs)
+    MSEs$rowMeans = rowMeans(MSEs)
     
     # adding the alphas and lambdas that we used
     # these are the same for every seed!
-    MSEs$lambdas <- ensr_obj_summary$lambda
-    MSEs$alphas<- ensr_obj_summary$alpha
-    MSEs <- MSEs[order(MSEs$rowMeans,decreasing = F), ]
+    MSEs$lambdas = ensr_obj_summary$lambda
+    MSEs$alphas= ensr_obj_summary$alpha
+    MSEs = MSEs[order(MSEs$rowMeans,decreasing = F), ]
     
     # Selecting the  alpha and the lambda of the best model
-    alpha.min <- MSEs$alphas[1]
-    lambda.min <- MSEs$lambdas[1]
+    alpha.min = MSEs$alphas[1]
+    lambda.min = MSEs$lambdas[1]
     
     # fitting the elastic net model and getting the estimates for the variables
-    elastic_model <- glmnet(y =data.matrix(y_train_entry), x = x_train_entry, family = 'binomial', alpha = alpha.min,
-                            lambda=lambda.min,standardize = F)
-    estimates <- elastic_model$beta
+    elastic_model = glmnet(y =data.matrix(y_train_entry), x = x_train_entry, family = ensr_family, alpha = alpha.min,
+                           lambda=lambda.min,standardize = F)
+    estimates = elastic_model$beta
     
     # having at least one parameter
     while (length(which(estimates!=0))<1){
-      MSEs <- MSEs[-1,]
-      lambda.min <- MSEs$lambdas[1]
-      alpha.min <- MSEs$alphas[1]
-      elastic_model <- glmnet(y =data.matrix(y_train_entry), x = x_train_entry, family = 'binomial',
-                              alpha = alpha.min,lambda=lambda.min,standardize = F)
-      estimates <- elastic_model$beta
+      MSEs = MSEs[-1,]
+      lambda.min = MSEs$lambdas[1]
+      alpha.min = MSEs$alphas[1]
+      elastic_model = glmnet(y =data.matrix(y_train_entry), x = x_train_entry, family = ensr_family,
+                             alpha = alpha.min,lambda=lambda.min,standardize = F)
+      estimates = elastic_model$beta
     }
     
     # calculate metrics
@@ -194,30 +218,59 @@ elastic_net_wrapper <- function(data, outcome=NULL, predictors_con=NULL,predicto
       if (sum(as.numeric(as.character(unlist(y_test_entry))))<stop_test){next}
     }
     
-    # AUC, sensitivity, specificity
-    predictions = predict(elastic_model, newx=x_test_entry,type = "response")
-    model_roc =  roc(unlist(y_test_entry),as.numeric(predictions),direction="<",quiet=T, levels=c('0','1'))
-    model_coords = coords(model_roc,"best", ret=c("threshold", "specificity", "sensitivity"), transpose=FALSE)
-    model_auc = auc(model_roc)
-    model_spec <- model_coords[2]
-    model_sens <- model_coords[3]
-    
-    # accuracy, PPV, NPV
-    predictions_bin = ifelse(predictions>model_coords$threshold,1,0)
-    confmatrix <- confusionMatrix(as.factor(predictions_bin),as.factor(unlist(y_test_entry)),positive='1')
-    
-    # storing metrics
-    results_df[entry,'fold']=entry
-    results_df[entry,'nrow_train']=nrow(x_train_entry)
-    results_df[entry,'nrow_test']=nrow(x_test_entry)
-    results_df[entry,'ny_train']=sum(as.numeric(as.character(unlist(y_train_entry))))
-    results_df[entry,'ny_test']=sum(as.numeric(as.character(unlist(y_test_entry))))
-    results_df[entry,'AUC']=model_auc
-    results_df[entry,'sensitivity']=model_sens
-    results_df[entry,'specificity']=model_spec
-    results_df[entry,'accuracy']=confmatrix$overall[1]
-    results_df[entry,'PPV']=confmatrix$byClass[3]
-    results_df[entry,'NPV']=confmatrix$byClass[4]
+    if (family=='binary'){
+      # AUC, sensitivity, specificity
+      predictions = predict(elastic_model, newx=x_test_entry,type = "response")
+      model_roc =  roc(unlist(y_test_entry),as.numeric(predictions),direction="<",quiet=T)
+      model_coords = coords(model_roc,"best", ret=c("threshold", "specificity", "sensitivity"), transpose=FALSE)
+      model_auc = auc(model_roc)
+      ifelse( nrow(model_coords[2])>1,model_spec <- NA, model_spec <- model_coords[2])
+      ifelse( nrow(model_coords[2])>1,model_sens <- NA, model_sens <- model_coords[3])
+      
+      # accuracy, PPV, NPV
+      predictions_bin = ifelse(predictions>model_coords$threshold,1,0)
+      confmatrix <- confusionMatrix(as.factor(predictions_bin),as.factor(unlist(y_test_entry)),positive='1')
+      
+      # storing metrics
+      results_df[entry,'fold']=entry
+      results_df[entry,'nrow_train']=nrow(x_train_entry)
+      results_df[entry,'nrow_test']=nrow(x_test_entry)
+      results_df[entry,'ny_train']=sum(as.numeric(as.character(unlist(y_train_entry))))
+      results_df[entry,'ny_test']=sum(as.numeric(as.character(unlist(y_test_entry))))
+      results_df[entry,'AUC']=model_auc
+      results_df[entry,'sensitivity']=model_sens
+      results_df[entry,'specificity']=model_spec
+      results_df[entry,'accuracy']=confmatrix$overall[1]
+      results_df[entry,'PPV']=confmatrix$byClass[3]
+      results_df[entry,'NPV']=confmatrix$byClass[4]
+      
+    }
+    else if (family=='continuous'){
+      
+      # Getting the predictions
+      predictions = predict(elastic_model, newx=x_test_entry,type = "response")
+      if (is.null(pred_min)==F){predictions[predictions<pred_min]=pred_min}
+      if (is.null(pred_max)==F){predictions[predictions>pred_max]=pred_max}
+      
+      # Getting R2, adjusted R2, RMSE, MSE, MAE
+      R2 = 1-(sum((y_test_entry-predictions)^2)/length(y_test_entry))/(sum((y_test_entry-mean(unlist(y_train_entry)))^2)/length(y_test_entry))
+      R2_adjusted = 1 - (1-R2)*(length(y_test_entry)-1)/(length(y_test_entry)-length(which(estimates!=0))-1)
+      RMSE = RMSE(y_test_entry,predictions)
+      MSE = RMSE^2
+      MAE = MAE(y_test_entry,predictions)
+      
+      # storing metrics
+      results_df[entry,'fold']=entry
+      results_df[entry,'nrow_train']=nrow(x_train_entry)
+      results_df[entry,'nrow_test']=nrow(x_test_entry)
+      results_df[entry,'mean_y_train']=mean(y_train_entry)
+      results_df[entry,'mean_y_test']=mean(y_test_entry)
+      results_df[entry,'R2']=R2
+      results_df[entry,'R2_adjusted']=R2_adjusted
+      results_df[entry,'RMSE']=RMSE
+      results_df[entry,'MSE']=MSE
+      results_df[entry,'MAE']=MAE
+    }
     
     # storing estimates
     for (predictor in predictors){
@@ -228,15 +281,13 @@ elastic_net_wrapper <- function(data, outcome=NULL, predictors_con=NULL,predicto
       else{
         results_df[entry,predictor]<- estimates[index]
       }
-      
-      # updating progress bar
-      setTxtProgressBar(pb,entry)
     }
+    # updating progress bar
+    setTxtProgressBar(pb,entry)
   }
   
   # close progress bar
   close(pb)
-  
   # remove stored variables
   rm(x_train_entry,envir = .GlobalEnv)
   rm(y_train_entry,envir = .GlobalEnv)
